79800c0ce632c3bf8cba06d359f390c85daebeae
marijabuhin <marija.buhin96@gmail.com>
2018-11-13 16:57:47 +0100

    navigation fixes

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,13 @@
+
 $(function () {
 
   $('.burger').on('click', function(){
-    $('.burger').toggleClass('burger--active');
+    $('.burger').addClass('burger--active');
+  	$(".nav--burger").animate({height: 'toggle'});
+  });
+
+  $( '.nav--burger a' ).on("click", function(){
+  	$('.burger').removeClass('burger--active');
     $(".nav--burger").animate({height: 'toggle'});
   });
 
